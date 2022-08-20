d63c407371c2e1c62c963e9f0a66835b8b6ac173
Vivek Shah <80442960+Vivek-Shah03@users.noreply.github.com>
2022-08-20 11:27:21 +0530

    Add files via upload

---
+++ studentmanagementsystembackend/src/main/java/in/ac/charusat/studentmgmtsystem/StudentMgmtSystemApplication.java
@@ -0,0 +1,11 @@
+package in.ac.charusat.studentmgmtsystem;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class StudentMgmtSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentMgmtSystemApplication.class, args);
+    }
+}
