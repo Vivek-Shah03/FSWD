1b03882114234b76d336cd0182178c8446135a1c
Vivek Shah <80442960+Vivek-Shah03@users.noreply.github.com>
2022-11-02 22:21:04 +0530

    Counter implemented

---
+++ dynamic-counter-in-react/src/App.js
@@ -0,0 +1,16 @@
+import "./styles.css";
+import { useState } from "react";
+export default function App() {
+  const [counter, setCounter] = useState(0);
+  return (
+    <div className="App">
+      <button onClick={() => setCounter((prevCounter) => prevCounter - 1)}>
+        -
+      </button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevCounter) => prevCounter + 1)}>
+        +
+      </button>
+    </div>
+  );
+}
